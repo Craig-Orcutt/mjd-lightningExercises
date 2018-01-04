cf0b94bc1fa3b58f17bf46a0f1f92b31647a3235
Craig Orcutt <craigorcutt@Craigs-MacBook-Pro.local>
2018-01-04 09:31:44 -0600

    practice for 1/4/18

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let ages  = [12, 23, 89, 16, 34, 19, 2, 7, 45, 50, 26, 102];
 let fave_demo = [];
 let fave_demo2 = [];
+// log ages that are between 17 and 50 using a for loop
 for(let i = 0; i < ages.length; i++){
     if(ages[i] >17 && ages[i] <50){
         fave_demo += ages[i] + ' ';
     };
 }
-
+// log ages that between 17 and 50 using a forEach
 ages.forEach((age) =>{
     if (age >17 && age <50){
         fave_demo2 += age + ' ';
@@ -15,7 +16,7 @@ ages.forEach((age) =>{
 
 
 
-
+// log ages between 17 and 50 using .filter
 let filterAge = ages.filter((age)=> age >17 && age <50  );
 
 
@@ -23,7 +24,7 @@ console.log(fave_demo);
 console.log(fave_demo2);
 console.log(filterAge);
 
-
+// console the 'meaning of life'
 hitchhikers_guide = {
     characters: ["Zaphod", "Arthur", "Ford", "Trillian"],
     catchphrase: "Don't Panic",
@@ -38,7 +39,7 @@ hitchhikers_guide = {
 
   console.log(hitchhikers_guide.random_facts.ultimate_answer.meaning_of_life);
 
-
+// capitalize 'The' and place a comma after 'not'
   let yoda_quote = ['the', 'greatest', 'teacher', 'failure', 'is'];
   console.log(yoda_quote.map((word) => {
       if (word === 'the')
@@ -47,4 +48,20 @@ hitchhikers_guide = {
         word = word + ' ,'
     return word    
   }). join (' '))
+
+
+
+
+//   Choose a loop method of your choice to get this result from this array:
+let newNumArr = [];
+let parseNum;
+let nums = [1, 2, 3, 4, "5", 6, "7"]
+// expected result: [3, 5, 7, 9, 11, 13]
+  for(i = 0; i < nums.length-1; i++){
+    newNumArr.push(+nums[i] + +nums[i + 1])
+}
+
+console.log(newNumArr);
+
+
 
