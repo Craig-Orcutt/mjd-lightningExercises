20ee63c9544be1008cec35d2a0e0554f2d6b9e03
Craig Orcutt <craigaorcutt@gmail.com>
2018-01-08 10:03:00 -0600

    lightning exercise for the day

--- main.js
+++ main.js
@@ -48,3 +48,58 @@ hitchhikers_guide = {
     return word    
   }). join (' '))
 
+
+  // lightning exercise:
+// Loop through this object and add each member object's key as an ID. Log the results.
+let customers = {
+    active: {
+      a87b6345: {
+        name: "Gary Harrison",
+        age: 23,
+        member_level: "gold"
+      },
+      b345hy66: {
+        name: "Harry Garrison",
+        age: 34,
+        member_level: "silver"
+      },
+      nh56yht8: {
+        name: "Mary Terryson",
+        age: 78,
+        member_level: "platinum"
+      },
+      j677yd456: {
+        name: "Barry Sonnyman",
+        age: 44,
+        member_level: "gold"
+      }
+    },
+    inactive: {
+      ur56tfgt: {
+        name: "Barb Justice",
+        age: 56,
+        member_level: "none"
+      },
+      d56gt6: {
+        name: "Jane L'eggo",
+        age: 11,
+        member_level: "none"
+      }
+    }
+  }
+
+  let idNumbers = []
+  let users = Object.keys(customers)
+  let active = customers.active;
+
+for(let status in customers){
+    let keys  = Object.keys(customers[status]);
+    console.log(keys);
+    keys.forEach( (key) =>{
+        customers[status][key].id = key;
+        console.log(key);
+        idNumbers.push(key);
+    });
+}
+console.log(idNumbers);
+  
